0d8263430fc6a10edafe455814a2fff7472ff9e4
hn219072 <96042095+hn219072@users.noreply.github.com>
2021-12-15 11:50:51 +0530

    Add files via upload

---
+++ himanshu py8.py
@@ -0,0 +1,21 @@
+a=2
+s=0
+while a<=20:
+    s=s+a
+    a=a+2
+print(s)
+
+#ap
+a=10
+while a<300:
+    print(a,end=",")
+    a=a+10
+    if a==300:
+        print(a)
+print("bye bye")
+
+a=1
+while a<=10:
+    print(a**2)
+    a=a+1
+print("bye bye")

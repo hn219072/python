936f6bb9c00d2ace0bd4ba4c58e9c1cd038b51e5
hn219072 <96042095+hn219072@users.noreply.github.com>
2021-12-22 13:15:54 +0530

    Add files via upload

---
+++ himanshu py12.py
@@ -0,0 +1,11 @@
+a=int(input("enter the no. of items puchased"))
+print("enter the cost of 1 item")
+b=int(input())
+print("enter the cost of 2 item")
+c=int(input())
+print("enter the cost of 3 item")
+d=int(input())
+print("total bill:",[b+c+d-(b+c+d)*30/100])
+
+
+    

e1232cac2ffe9b1b6e4ae0a4cd4cf7d367cb031b
hn219072 <96042095+hn219072@users.noreply.github.com>
2021-12-27 12:46:00 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,9 @@
+#triangle
+for i in range(5):
+    for j in range(i+1):
+        print("*",end=" ")
+    print(" ")
+for i in range(5,1,-1):
+    for j in range(i-1):
+        print("*",end=" ")
+    print(" ")

12bc9d9e2d24a9809a01fc708322513354d0fe76
hn219072 <96042095+hn219072@users.noreply.github.com>
2021-12-27 12:27:51 +0530

    Add files via upload

---
+++ himanshu 14.py
@@ -0,0 +1,10 @@
+#a
+for i in range(7,2,-1):
+    for j in range(1,i-1):
+        print(j,end=" ")
+    print("\r")
+#b
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print("\r")    
